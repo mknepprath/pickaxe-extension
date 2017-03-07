9d68892782afefe5f611cc211d73dfc77bc315f6
mknepprath <mknepprath@gmail.com>
2017-03-07 13:58:13 -0600

    port hide/show, renaming, major refac of popup.js

--- source/pickaxe.js
+++ source/pickaxe.js
@@ -1,11 +1,11 @@
-const {location: {href: currentUrl}} = window
+const {location: {href: currentUrl}, localStorage} = window
 const editorId = currentUrl.substr(currentUrl.lastIndexOf('/') + 1)
-const {localStorage, localStorage: {[editorId]: token}} = window
+const {localStorage: {[editorId]: token}} = window
 const url = 'http://localhost:3000/editor/' + editorId + '?token=' + token
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.storage === 'clear') {
+  function (request, sender, sendResponse) {
+    if (request.storage) {
       window.localStorage.clear()
       chrome.storage.local.clear()
       sendResponse({status: 'complete'})
@@ -20,9 +20,15 @@ chrome.runtime.onMessage.addListener(
 // Pushes all editor ids and tokens from localStorage to storage (ids are 24 characters long)
 for (let editor in localStorage) {
   if (editor.length === 24) {
-    chrome.storage.local.get(editor, function (obj) {
-      if (Object.keys(obj).length === 0 && obj.constructor === Object) {
-        chrome.storage.local.set({[editor]: localStorage[editor]})
+    chrome.storage.local.get(editor, function (storedEditor) {
+      // If stored editor obj is empty, set new editor from localStorage
+      if (!Object.keys(storedEditor).length) {
+        chrome.storage.local.set({
+          [editor]: {
+            token: localStorage[editor],
+            name: editor
+          }
+        })
       }
     })
   }
@@ -31,36 +37,29 @@ for (let editor in localStorage) {
 // Creates and places corner shortcut buttons
 const emoji = ['✨', '🌟', '💫', '☄', '🚀', '⛏']
 
-var pickaxeLink = document.createElement('div')
+let pickaxeLink = document.createElement('div')
 pickaxeLink.className = 'pickaxe-ext-link'
 pickaxeLink.innerHTML = url
 document.body.appendChild(pickaxeLink)
 
-var pickaxeBtn = document.createElement('button')
-pickaxeBtn.className = 'pickaxe-ext-btn'
-pickaxeBtn.innerHTML = '🚀'
-document.body.appendChild(pickaxeBtn)
-
-var localBtn = document.createElement('button')
-localBtn.className = 'pickaxe-ext-local-btn'
-localBtn.innerHTML = '🏠'
-document.body.appendChild(localBtn)
-
-localBtn.onclick = function () {
-  window.open(url,'_blank')
-  localBtn.innerHTML = '🏚'
-}
-
-pickaxeBtn.onclick = function () {
-  var range = document.createRange()
+let pickaxeCopy = document.createElement('button')
+pickaxeCopy.className = 'pickaxe-ext-btn'
+pickaxeCopy.innerHTML = '🚀'
+pickaxeCopy.onclick = function () {
+  let range = document.createRange()
   range.selectNode(pickaxeLink)
   window.getSelection().addRange(range)
-  try {
-    // Now that we've selected the anchor text, execute the copy command
-    document.execCommand('copy')
-    pickaxeBtn.innerHTML = emoji[Math.floor(Math.random() * emoji.length)]
-  } catch(err) {
-    console.log('Oops, unable to copy')
-  }
+  document.execCommand('copy')
   window.getSelection().removeAllRanges()
+  pickaxeCopy.innerHTML = emoji[Math.floor(Math.random() * emoji.length)]
 }
+document.body.appendChild(pickaxeCopy)
+
+let pickaxeOpen = document.createElement('button')
+pickaxeOpen.className = 'pickaxe-ext-local-btn'
+pickaxeOpen.innerHTML = '🏠'
+pickaxeOpen.onclick = function () {
+  window.open(url,'_blank')
+  pickaxeOpen.innerHTML = '🏚'
+}
+document.body.appendChild(pickaxeOpen)
--- source/popup.js
+++ source/popup.js
@@ -1,102 +1,168 @@
 // Initialization of the popup
-document.getElementById('clear-storage').onclick = function(){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {storage: 'clear'}, function(response) {
+const urlBases = document.getElementById('urlBase')
+const port = document.getElementById('port')
+const clearStorage = document.getElementById('clear-storage')
+
+chrome.storage.local.get(null, function (editors) {
+  for (let id in editors) {
+    // Create editor listing
+    let editorItem = document.createElement('li')
+    editorItem.id = id
+    let editorName = document.createElement('input')
+    editorName.className = 'editor-name'
+    editorName.value = editors[id].name
+    editorName.size = editors[id].name.length + 3
+    editorName.onchange = updateEditorName
+    editorItem.append(editorName)
+    editorItem.append(
+      createButton(
+        'delete',
+        '&times;',
+        deleteEditor
+      )
+    )
+    editorItem.append(
+      createButton(
+        'copy',
+        'copy',
+        copy
+      )
+    )
+    editorItem.append(
+      createButton(
+        'open',
+        'open',
+        open
+      )
+    )
+
+    // Add editor listing to popup
+    document.getElementById('storage').prepend(editorItem)
+
+    // Adds hidden divs with links for copying
+    document.body.appendChild(
+      createPickaxeLink(
+        urlBases.options[urlBases.selectedIndex].value,
+        document.getElementById('port').value,
+        id,
+        editors[id].token
+      )
+    )
+  }
+})
+
+// Highlight current tab
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  const currentUrl = tabs[0].url
+  const currentId = currentUrl.substr(currentUrl.lastIndexOf('/') + 1)
+  document.getElementById(currentId).className = 'current'
+})
+
+// Clears storage when Clear Editors button is clicked
+clearStorage.onclick = function () {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {storage: 'clear'}, function (response) {
       if (response.status === 'complete') {
-        getEditors()
+        document.getElementById('storage').innerHTML = ''
+        displayClearStorage()
       }
     })
   })
 }
 
-const getEditors = function () {
-  chrome.storage.local.get(null, function(editors) {
-    document.getElementById('storage').innerHTML = ''
+// Updates display of port input when url base is changed
+urlBases.onchange = function () {
+  const selectedBase = this.options[this.selectedIndex]
+  displayPort(selectedBase)
+  updatePickaxeLinks(
+    selectedBase.value,
+    document.getElementById('port').value
+  )
+}
 
+port.onchange = function () {
+  updatePickaxeLinks(
+    urlBases.options[urlBases.selectedIndex].value,
+    this.value
+  )
+}
+
+const updateEditorName = function () {
+  const {id} = this.parentNode
+  const newEditorName = this.value
+  chrome.storage.local.get(id, function (editors) {
+    chrome.storage.local.set({
+      [id]: {
+        token: editors[id].token,
+        name: newEditorName
+      }
+    })
+  })
+}
+
+const createButton = function (className, text, onclick) {
+  let btn = document.createElement('button')
+  btn.className = className
+  btn.innerHTML = text
+  btn.onclick = onclick
+  return btn
+}
+
+const createPickaxeLink = function (base, port, id, token) {
+  let pickaxeLink = document.createElement('div')
+  pickaxeLink.className = 'pickaxe-copy-link'
+  pickaxeLink.id = 'copy-' + id
+  pickaxeLink.innerHTML = 'http://' + base + (base.endsWith(':') ? port : '') + '/editor/' + id + '?token=' + token
+  return pickaxeLink
+}
+
+const updatePickaxeLinks = function (base, port) {
+  chrome.storage.local.get(null, function (editors) {
     for (let id in editors) {
-      const token = editors[id]
-
-      // Create editor listing
-      let editorItem = document.createElement('li')
-
-      // Create button that opens editor
-      let editorOpenBtn = document.createElement('button')
-      editorOpenBtn.className = 'open'
-      editorOpenBtn.innerHTML = 'open'
-      editorOpenBtn.onclick = function () {
-        const urlBases = document.getElementById('urlBase')
-        let urlBase = urlBases.options[urlBases.selectedIndex].value
-        if (urlBase.endsWith(':'))
-          urlBase += document.getElementById('port').value
-        window.open('http://' + urlBase + '/editor/' + id + '?token=' + token, '_blank')
-      }
-
-      const urlBases = document.getElementById('urlBase')
-      for (base in urlBases.options) {
-        if (urlBases.options[base].value !== undefined) {
-          // Create link for copy button
-          let pickaxeLink = document.createElement('div')
-          pickaxeLink.className = 'pickaxe-copy-link'
-          const urlBase = urlBases.options[base].value
-          pickaxeLink.id = urlBase + '-' + id
-          pickaxeLink.innerHTML = 'http://' + urlBase + '/editor/' + id + '?token=' + token
-          document.body.appendChild(pickaxeLink)
-        }
-      }
-
-      // Create button that copies editor url + token
-      let editorCopyBtn = document.createElement('button')
-      editorCopyBtn.className = 'copy'
-      editorCopyBtn.innerHTML = 'copy'
-      editorCopyBtn.onclick = function () {
-        var range = document.createRange()
-        const urlBases = document.getElementById('urlBase')
-        const urlBase = urlBases.options[urlBases.selectedIndex].value
-        const pickaxeLink = document.getElementById(urlBase + '-' + id)
-        range.selectNode(pickaxeLink)
-        window.getSelection().addRange(range)
-        try {
-          // Now that we've selected the anchor text, execute the copy command
-          document.execCommand('copy')
-        } catch(err) {
-          console.log('Oops, unable to copy')
-        }
-        window.getSelection().removeAllRanges()
-      }
-
-      // Create button that deletes editor sessions
-      let editorDeleteBtn = document.createElement('button')
-      editorDeleteBtn.className = 'delete'
-      editorDeleteBtn.innerHTML = '&times;'
-      editorDeleteBtn.onclick = function () {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {remove: id}, function(response) {
-            if (response.status === 'complete') {
-              editorItem.parentNode.removeChild(editorItem)
-            }
-          })
-        })
-      }
-
-      // Get current tab
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        let currentUrl = tabs[0].url
-        let currentId = currentUrl.substr(currentUrl.lastIndexOf('/') + 1)
-        if (id === currentId) {
-          editorItem.className = 'current'
-        }
-      })
-
-      // Add elements to editor listing
-      editorItem.append(id)
-      editorItem.append(editorDeleteBtn)
-      editorItem.append(editorCopyBtn)
-      editorItem.append(editorOpenBtn)
-
-      // Add editor listing to popup
-      document.getElementById('storage').prepend(editorItem)
+      document.getElementById('copy-' + id).innerHTML = 'http://' + base + (base.endsWith(':') ? port : '') + '/editor/' + id + '?token=' + editors[id].token
     }
   })
 }
 
-getEditors()
+const displayPort = function (base) {
+  const displayPort = base.value.endsWith(':')
+  document.getElementById('port-section').style.display = displayPort ? 'inline-block' : 'none'
+}
+
+const displayClearStorage = function () {
+  chrome.storage.local.get(null, function (editors) {
+    clearStorage.style.display = (!Object.keys(editors).length) ? 'none' : 'block'
+  })
+}
+
+const open = function () {
+  const {id} = this.parentNode
+  const selectedBase = urlBases.options[urlBases.selectedIndex]
+  const portRequired = selectedBase.value.endsWith(':')
+  chrome.storage.local.get(id, function (editors) {
+    window.open('http://' + selectedBase.value + (portRequired ? document.getElementById('port').value : '') + '/editor/' + id + '?token=' + editors[id].token, '_blank')
+  })
+}
+
+const copy = function () {
+  const {id} = this.parentNode
+  const selectedBase = urlBases.options[urlBases.selectedIndex]
+  const pickaxeLink = document.getElementById('copy-' + id)
+  let range = document.createRange()
+  range.selectNode(pickaxeLink)
+  window.getSelection().addRange(range)
+  document.execCommand('copy')
+  window.getSelection().removeAllRanges()
+}
+
+const deleteEditor = function () {
+  const {id} = this.parentNode
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {remove: id}, function (response) {
+      if (response.status === 'complete') {
+        const deletion = document.getElementById(id)
+        deletion.parentNode.removeChild(deletion)
+      }
+    })
+  })
+}
