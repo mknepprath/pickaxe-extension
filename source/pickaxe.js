d44b96873b7cc62bdbbea53ae647df0e798755f6
mknepprath <mknepprath@gmail.com>
2017-03-07 18:00:56 -0600

    refactor pickaxe.js, remove buttons

--- source/pickaxe.js
+++ source/pickaxe.js
@@ -1,15 +1,13 @@
-const {location: {href: currentUrl}, localStorage} = window
-const editorId = currentUrl.substr(currentUrl.lastIndexOf('/') + 1)
-const {localStorage: {[editorId]: token}} = window
-const url = 'http://localhost:3000/editor/' + editorId + '?token=' + token
-
+// handles messaging from the popup
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.storage) {
+      // clear storage
       window.localStorage.clear()
       chrome.storage.local.clear()
       sendResponse({status: 'complete'})
     } else if (request.remove) {
+      // remove specific editor
       delete window.localStorage[request.remove]
       chrome.storage.local.remove(request.remove)
       sendResponse({status: 'complete'})
@@ -18,14 +16,14 @@ chrome.runtime.onMessage.addListener(
 )
 
 // Pushes all editor ids and tokens from localStorage to storage (ids are 24 characters long)
-for (let editor in localStorage) {
+for (let editor in window.localStorage) {
   if (editor.length === 24) {
     chrome.storage.local.get(editor, function (storedEditor) {
       // If stored editor obj is empty, set new editor from localStorage
       if (!Object.keys(storedEditor).length) {
         chrome.storage.local.set({
           [editor]: {
-            token: localStorage[editor],
+            token: window.localStorage[editor],
             name: editor
           }
         })
@@ -33,33 +31,3 @@ for (let editor in localStorage) {
     })
   }
 }
-
-// Creates and places corner shortcut buttons
-const emoji = ['✨', '🌟', '💫', '☄', '🚀', '⛏']
-
-let pickaxeLink = document.createElement('div')
-pickaxeLink.className = 'pickaxe-ext-link'
-pickaxeLink.innerHTML = url
-document.body.appendChild(pickaxeLink)
-
-let pickaxeCopy = document.createElement('button')
-pickaxeCopy.className = 'pickaxe-ext-btn'
-pickaxeCopy.innerHTML = '🚀'
-pickaxeCopy.onclick = function () {
-  let range = document.createRange()
-  range.selectNode(pickaxeLink)
-  window.getSelection().addRange(range)
-  document.execCommand('copy')
-  window.getSelection().removeAllRanges()
-  pickaxeCopy.innerHTML = emoji[Math.floor(Math.random() * emoji.length)]
-}
-document.body.appendChild(pickaxeCopy)
-
-let pickaxeOpen = document.createElement('button')
-pickaxeOpen.className = 'pickaxe-ext-local-btn'
-pickaxeOpen.innerHTML = '🏠'
-pickaxeOpen.onclick = function () {
-  window.open(url,'_blank')
-  pickaxeOpen.innerHTML = '🏚'
-}
-document.body.appendChild(pickaxeOpen)
